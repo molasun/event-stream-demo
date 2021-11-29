587943907f6db88b3385442ada9d5138f69831ed
molasun <mola79321@gmail.com>
2021-11-29 10:19:46 +0800

    modify edge simulator

---
+++ projects/harvest/display/ConsoleService.java
@@ -0,0 +1,74 @@
+// camel-k: dependency=camel-infinispan dependency=camel-bean dependency=camel-jackson dependency=camel-swagger-java dependency=mvn:org.wildfly.security:wildfly-elytron:1.11.2.Final dependency=mvn:io.netty:netty-codec:4.1.49.Final configmap=shippingconsole-config trait=quarkus.enabled=false
+package module3;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.infinispan.InfinispanConstants;
+import org.apache.camel.component.infinispan.InfinispanOperation;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+//import org.apache.camel.component.jackson.JacksonDataFormat;
+import org.apache.camel.model.rest.RestBindingMode;
+
+//oc delete configmap shippingconsole-config  
+//oc apply -f shippingconsole-config.yaml
+// 
+//kamel run ConsoleService.java --dev
+
+public class ConsoleService extends RouteBuilder{
+
+
+  
+    @Override
+    public void configure() throws Exception{
+
+        restConfiguration()
+            .apiContextPath("/api-doc")
+            .apiProperty("cors", "true")
+            .apiProperty("api.title", "Order API")
+            .apiProperty("api.version", "1.0")
+            .enableCORS(true)
+            .port("8080")
+            .bindingMode(RestBindingMode.json);
+
+        rest()
+            .get("/query")
+                .to("direct:query")
+            .get("/premium")
+                .to("direct:premium")
+            .get("/standard")
+                .to("direct:standard")
+        ;
+
+        from("direct:premium")
+            .log("GETTING premium")
+            .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
+            .setHeader(InfinispanConstants.KEY).constant("premium")
+            .to("infinispan:default")
+            //.to("mustache:premium.mustache")         
+            .log("${body}")
+        ;
+
+        from("direct:standard")
+            .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
+            .setHeader(InfinispanConstants.KEY).constant("standard")
+            .to("infinispan:default")         
+            .log("${body}")
+        ;
+
+        
+    
+        
+        
+    }
+
+
+
+       
+
+
+
+        
+}
---
+++ projects/harvest/shipping/PremiumShipping.java
@@ -0,0 +1,57 @@
+// camel-k: dependency=camel-infinispan dependency=camel-bean dependency=camel-jackson dependency=mvn:org.wildfly.security:wildfly-elytron:1.11.2.Final dependency=mvn:io.netty:netty-codec:4.1.49.Final configmap=premiumshipping-config trait=quarkus.enabled=false
+
+package module3;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.infinispan.InfinispanConstants;
+import org.apache.camel.component.infinispan.InfinispanOperation;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+
+public class PremiumShipping extends RouteBuilder{
+
+    static final int MAX_PER_CARGO=10;
+
+
+    @Override
+    public void configure() throws Exception{
+
+    /*
+     * Insert Camel routes here
+     */
+    }
+        public Map assignShipment(Map<Integer, List<Map>> premiumShipment, Map marshmallow){
+
+            if(premiumShipment == null)
+                premiumShipment = new HashMap<Integer, List<Map>>();
+
+            premiumShipment = addCargo(premiumShipment,marshmallow);
+
+            return premiumShipment;
+
+        }
+
+        private Map<Integer, List<Map>> addCargo(Map<Integer, List<Map>> premiumShipment, Map marshmallow){
+            int lastCargoNo = 100;
+            for(Map.Entry<Integer, List<Map>> item:premiumShipment.entrySet()){
+                lastCargoNo = item.getKey();
+                if(item.getValue().size() < MAX_PER_CARGO){
+                    item.getValue().add(marshmallow);
+                    return premiumShipment;
+                }
+            }
+
+            List newcargo = new ArrayList<Map>();
+            newcargo.add(marshmallow);
+            premiumShipment.put(lastCargoNo+1, newcargo);
+            return premiumShipment;
+        }
+
+
+
+
+}
