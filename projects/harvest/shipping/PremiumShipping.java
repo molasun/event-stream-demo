f7c95c0debe860d53038d1f3d1826ffe804e0933
molasun <mola79321@gmail.com>
2021-11-29 16:28:01 +0800

    update shipping service

--- projects/harvest/shipping/PremiumShipping.java
+++ projects/harvest/shipping/PremiumShipping.java
@@ -1,6 +1,7 @@
 // camel-k: dependency=camel-infinispan dependency=camel-bean dependency=camel-jackson dependency=mvn:org.wildfly.security:wildfly-elytron:1.11.2.Final dependency=mvn:io.netty:netty-codec:4.1.49.Final configmap=premiumshipping-config trait=quarkus.enabled=false
 
 package module3;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
@@ -12,46 +13,53 @@ import java.util.ArrayList;
 import java.util.function.Consumer;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 
-public class PremiumShipping extends RouteBuilder{
+public class PremiumShipping extends RouteBuilder {
 
-    static final int MAX_PER_CARGO=10;
-
+    static final int MAX_PER_CARGO = 10;
 
     @Override
-    public void configure() throws Exception{
+    public void configure() throws Exception {
 
-    /*
-     * Insert Camel routes here
-     */
+        from("timer:cleanup?repeatCount=1").setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.CLEAR)
+                .setHeader(InfinispanConstants.KEY).constant("premium").to("infinispan:default");
+
+        from("kafka:premium?groupId=premium-shipping").streamCaching().unmarshal(new JacksonDataFormat(Map.class))
+                .log("Input --> ${body}").setHeader("marshmallow").simple("${body}")
+                .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
+                .setHeader(InfinispanConstants.KEY).constant("premium").to("infinispan:default")
+                .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
+                .setHeader(InfinispanConstants.KEY).constant("premium").setHeader(InfinispanConstants.VALUE)
+                .method(this, "assignShipment(${body}, ${header.marshmallow})").log("${body}").to("infinispan:default")
+
+        ;
+
     }
-        public Map assignShipment(Map<Integer, List<Map>> premiumShipment, Map marshmallow){
 
-            if(premiumShipment == null)
-                premiumShipment = new HashMap<Integer, List<Map>>();
+    public Map assignShipment(Map<Integer, List<Map>> premiumShipment, Map marshmallow) {
 
-            premiumShipment = addCargo(premiumShipment,marshmallow);
+        if (premiumShipment == null)
+            premiumShipment = new HashMap<Integer, List<Map>>();
 
-            return premiumShipment;
+        premiumShipment = addCargo(premiumShipment, marshmallow);
 
-        }
+        return premiumShipment;
 
-        private Map<Integer, List<Map>> addCargo(Map<Integer, List<Map>> premiumShipment, Map marshmallow){
-            int lastCargoNo = 100;
-            for(Map.Entry<Integer, List<Map>> item:premiumShipment.entrySet()){
-                lastCargoNo = item.getKey();
-                if(item.getValue().size() < MAX_PER_CARGO){
-                    item.getValue().add(marshmallow);
-                    return premiumShipment;
-                }
+    }
+
+    private Map<Integer, List<Map>> addCargo(Map<Integer, List<Map>> premiumShipment, Map marshmallow) {
+        int lastCargoNo = 100;
+        for (Map.Entry<Integer, List<Map>> item : premiumShipment.entrySet()) {
+            lastCargoNo = item.getKey();
+            if (item.getValue().size() < MAX_PER_CARGO) {
+                item.getValue().add(marshmallow);
+                return premiumShipment;
             }
-
-            List newcargo = new ArrayList<Map>();
-            newcargo.add(marshmallow);
-            premiumShipment.put(lastCargoNo+1, newcargo);
-            return premiumShipment;
         }
 
-
-
+        List newcargo = new ArrayList<Map>();
+        newcargo.add(marshmallow);
+        premiumShipment.put(lastCargoNo + 1, newcargo);
+        return premiumShipment;
+    }
 
 }
