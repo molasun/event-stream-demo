182a3c265f387c58562cdeca999ca06f1856cb2a
molasun <mola79321@gmail.com>
2021-11-29 17:03:05 +0800

    modify kafka topic name

--- projects/harvest/shipping/StandardShipping.java
+++ projects/harvest/shipping/StandardShipping.java
@@ -1,6 +1,7 @@
 // camel-k: dependency=camel-infinispan dependency=camel-bean dependency=camel-jackson dependency=mvn:org.wildfly.security:wildfly-elytron:1.11.2.Final dependency=mvn:io.netty:netty-codec:4.1.49.Final configmap=standardshipping-config trait=quarkus.enabled=false
 
 package module3;
+
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
@@ -18,40 +19,30 @@ import org.apache.camel.component.jackson.JacksonDataFormat;
 //kamel run -d camel-infinispan -d camel-bean -d camel-jackson -d mvn:org.wildfly.security:wildfly-elytron:1.11.2.Final --configmap standardshipping-config StandardShipping.java --dev
 //--trait debug.enabled=true --property logging.level.org.apache.camel=DEBUG
 
-public class StandardShipping extends RouteBuilder{
+public class StandardShipping extends RouteBuilder {
 
-    static final int MAX_PER_CARGO=100;
+    static final int MAX_PER_CARGO = 100;
 
-  
     @Override
-    public void configure() throws Exception{
+    public void configure() throws Exception {
 
-        from("timer:cleanup?repeatCount=1")
-            .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.CLEAR)
-            .setHeader(InfinispanConstants.KEY).constant("standard")
-            .to("infinispan:default")         
-        ;
+        from("timer:cleanup?repeatCount=1").setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.CLEAR)
+                .setHeader(InfinispanConstants.KEY).constant("standard").to("infinispan:default");
 
-        //USE API
-        from("kafka:user1-premium?groupId=standard-shipping")
-            .setHeader("marshmallow").simple("${body}")
-            .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
-            .setHeader(InfinispanConstants.KEY).constant("standard")
-            .to("infinispan:default")         
-            .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
-            .setHeader(InfinispanConstants.KEY).constant("standard")
-            .setHeader(InfinispanConstants.VALUE).method(this, "assignShipment(${body})")
-            .log("${body}")
-            .to("infinispan:default")
-            
+        // USE API
+        from("kafka:standard?groupId=standard-shipping").setHeader("marshmallow").simple("${body}")
+                .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
+                .setHeader(InfinispanConstants.KEY).constant("standard").to("infinispan:default")
+                .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
+                .setHeader(InfinispanConstants.KEY).constant("standard").setHeader(InfinispanConstants.VALUE)
+                .method(this, "assignShipment(${body})").log("${body}").to("infinispan:default")
+
         ;
     }
 
+    public Map assignShipment(Map<Integer, Integer> standardShipment) {
 
-
-    public Map assignShipment(Map<Integer, Integer> standardShipment){
-            
-        if(standardShipment == null)
+        if (standardShipment == null)
             standardShipment = new HashMap<Integer, Integer>();
 
         standardShipment = addCargo(standardShipment);
@@ -59,19 +50,18 @@ public class StandardShipping extends RouteBuilder{
 
     }
 
-    private Map<Integer, Integer> addCargo(Map<Integer, Integer> standardShipment){
-            int lastCargoNo = 100;
-            for(Map.Entry<Integer, Integer> item:standardShipment.entrySet()){
-                lastCargoNo = item.getKey();
-                if(item.getValue() < MAX_PER_CARGO){
-                    item.setValue(item.getValue()+1);
-                    return standardShipment;
-                }
+    private Map<Integer, Integer> addCargo(Map<Integer, Integer> standardShipment) {
+        int lastCargoNo = 100;
+        for (Map.Entry<Integer, Integer> item : standardShipment.entrySet()) {
+            lastCargoNo = item.getKey();
+            if (item.getValue() < MAX_PER_CARGO) {
+                item.setValue(item.getValue() + 1);
+                return standardShipment;
             }
-
-            standardShipment.put(lastCargoNo+1, 1);
-            return standardShipment;
         }
 
+        standardShipment.put(lastCargoNo + 1, 1);
+        return standardShipment;
+    }
 
 }
