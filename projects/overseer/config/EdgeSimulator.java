d823ceb2e480fc9b98a0ed1080f42220ce3b3a60
molasun <mola79321@gmail.com>
2021-12-04 12:03:11 +0800

    Update overseer edge simulator service

--- projects/overseer/config/EdgeSimulator.java
+++ projects/overseer/config/EdgeSimulator.java
@@ -9,38 +9,38 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EdgeSimulator extends RouteBuilder{
+public class EdgeSimulator extends RouteBuilder {
 
-  public static final int MIN = 150;
-  @Override
-  public void configure() throws Exception{
+    public static final int MIN = 1;
 
-  from("timer:tick?fixedRate=true&period=5000")
-.choice()
-    .when(simple("{{simulator.run}}"))
-        .setBody(method(this, "genRandomIoTData()"))
-        .marshal().json()
-        .log("${body}")
-        .to("amqp:topic:mytopic?subscriptionDurable=false&exchangePattern=InOnly")
-    .otherwise()
-        .log("Nothing send ")
-;
+    @Override
+    public void configure() throws Exception {
 
-  }
+        from("timer:tick?fixedRate=true&period=5000")
+                .choice()
+                .when(simple("{{simulator.run}}"))
+                .setBody(method(this, "genRandomIoTData()"))
+                .marshal().json()
+                .log("${body}")
+                .to("amqp:topic:mytopic?subscriptionDurable=false&exchangePattern=InOnly")
+                .otherwise()
+                .log("Nothing send ");
 
-    public Map genRandomIoTData(){
+    }
+
+    public Map genRandomIoTData() {
         Random generator = new Random();
-        Map iotData = new HashMap<String,Object>();
+        Map iotData = new HashMap<String, Object>();
 
-        Integer[] farms = {101, 302, 787, 645, 555, 460, 892};
+        Integer[] farms = { 101, 302, 787, 645, 555, 460, 892 };
         int randomIndex = generator.nextInt(farms.length);
-        int batchcnt =  generator.nextInt(87) + MIN;
+        int batchcnt = generator.nextInt(87) + MIN;
         long batchtime = System.currentTimeMillis();
 
         List<Map> harvest = new ArrayList<Map>();
 
         for (int i = 0; i < batchcnt; i++) {
-            harvest.add(genSingleEvent((batchtime*10)+i));
+            harvest.add(genSingleEvent((batchtime * 10) + i));
         }
 
         iotData.put("farmid", farms[randomIndex]);
@@ -51,12 +51,12 @@ public class EdgeSimulator extends RouteBuilder{
         return iotData;
     }
 
-    private Map genSingleEvent(long eventid){
+    private Map genSingleEvent(long eventid) {
 
-        Map harvestEvent = new HashMap<String,Integer>();
+        Map harvestEvent = new HashMap<String, Integer>();
         Random generator = new Random();
         harvestEvent.put("diameter", (generator.nextInt(5) + 1));
-        harvestEvent.put("weight", ((generator.nextInt(5) + 1.0) + (((int)generator.nextInt(8)+1)*0.1)));
+        harvestEvent.put("weight", ((generator.nextInt(5) + 1.0) + (((int) generator.nextInt(8) + 1) * 0.1)));
         harvestEvent.put("mmid", eventid);
 
         return harvestEvent;
